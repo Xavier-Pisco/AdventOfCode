aa11aa97dea350f9b424e4b102933f0c05a99e37
Xavier-Pisco <x.pisco@gmail.com>
2020-12-02 08:31:55 +0000

    finished 1st day

--- 01.ts
+++ 01.ts
@@ -1,12 +1,22 @@
 import * as files from 'fs';
 
+let input: number[] = files.readFileSync('01.txt').toString().split('\n').map(Number);
 
-let input : number[] = files.readFileSync('01.txt').toString().split('\n').map(Number);
-
-for (let i : number = 0; i < input.length - 1; i++){
-	for (let j : number = i; j < input.length; j++){
+for (let i: number = 0; i < input.length - 1; i++) {
+	for (let j: number = i + 1; j < input.length; j++) {
 		if (input[i] + input[j] === 2020) {
-			console.log(input[i] + ' * '  + input[j] + ' = ' + input[i] * input[j]);
+			console.log(input[i] + ' * ' + input[j] + ' = ' + input[i] * input[j]);
+		}
+	}
+}
+
+
+for (let i: number = 0; i < input.length - 2; i++) {
+	for (let j: number = i + 1; j < input.length - 1; j++) {
+		for (let x: number = j + 1; x < input.length; x++) {
+			if (input[i] + input[j] + input[x] === 2020) {
+				console.log(input[i] + ' * ' + input[j] + ' * ' + input[x] + ' = ' + input[i] * input[j] * input[x]);
+			}
 		}
 	}
 }
