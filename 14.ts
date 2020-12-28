2105b14c68252b8705bc017594c6d46ea6274700
Xavier-Pisco <x.pisco@gmail.com>
2020-12-28 01:17:28 +0000

    Don't know what's wrong with 2nd part day 14

--- 14.ts
+++ 14.ts
@@ -1,6 +1,6 @@
 import * as files from 'fs';
 
-let input = files.readFileSync('14.txt').toString().split('mask = ').map((a) => a.split(new RegExp('\nmem\\[')).map((a) => a.split(new RegExp('\\] = '))));
+let input = files.readFileSync('14.txt').toString().split(new RegExp('\n?mask = ')).map((a) => a.split(new RegExp('\nmem\\[')).map((a) => a.split(new RegExp('\\] = '))));
 input.splice(0, 1);
 
 
@@ -39,34 +39,53 @@ for (let number of numbers) {
 
 console.log('1st Part: ' + final);
 
-/*
-let addresses = [];
+numbers = [];
 
-function getAddresses(mask: string[], value: number) {
-	let finalMask: string[] = [];
+function simplify(mask: string[], address: number) {
 	for (let i = 0; i < mask.length; i++) {
 		if (mask[i] === '0') {
-			finalMask[i] = getNumberAtPosition(value, 36 - i - 1).toString();
+			mask[i] = (getNumberAtPosition(address, 36 - i - 1)).toString();
 		} else if (mask[i] === '1') {
-			finalMask[i] = '1';
-		} else if (mask[i] === 'X') {
-			finalMask[i] = '0';
+			mask[i] = '1';
 		}
 	}
-	return finalMask.join('');
+	return mask.join('');
 }
 
+function setAddresses(mask: string[], address: number, value: number) {
+	for (let i = 0; i < mask.length; i++) {
+		if (mask[i] === 'X') {
+			mask[i] = '0';
+			setAddresses(Array.from(mask), address, value);
+			mask[i] = '1';
+		}
+
+	}
+
+	addresses.push(parseInt(mask.join(''), 2));
+	numbers[parseInt(mask.join(''), 2)] = value;
+}
+
+let addresses = [];
 
 for (let i = 0; i < input.length; i++) {
-	const mask = input[i][0][0];
+	let mask = input[i][0][0];
 	for (let j = 1; j < input[i].length; j++) {
-		const address = Number(input[i][j][0]);
-		const decoded = getAddresses(mask.split(''), address);
+		let address = Number(input[i][j][0]);
+		mask = simplify(mask.split(''), address);
+		setAddresses(mask.split(''), address, Number(input[i][j][1]));
 	}
 }
 
-for (let i = 0; i < addresses.length; i++){
-	addresses[i] = addresses[i].join('');
+final = 0;
+
+addresses.sort((a, b) => a - b);
+
+for (let i = 0; i < addresses.length; i++) {
+	if (addresses[i] === addresses[i - 1])
+		continue;
+
+	final += numbers[addresses[i]];
 }
-console.log(addresses);
-*/
+
+console.log('2nd Part: ' + final);
